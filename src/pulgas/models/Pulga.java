3f0b528a50860587cce1ddb9b0b23de02cd0ef57
Sebastian Arias <juansearias21@gmail.com>
2025-05-02 09:34:31 -0500

    clase pulga creada

--- src/pulgas/models/Pulga.java
+++ src/pulgas/models/Pulga.java
@@ -7,7 +7,60 @@ package pulgas.models;
 /**
  *
  * @author juans
+ *
+ * Clase abstracta que define el comportamiento base de las pulgas
  */
-public class Pulga {
+abstract class Pulga {
+    protected int x, y;
+    protected int ancho, alto;
+    protected Image imagen;
     
+    /**
+     * Dibuja la pulga en el componente gráfico
+     */
+    public void dibujar(Graphics g) {
+        g.drawImage(imagen, x, y, null);
+    }
+    
+    /**
+     * Maneja la acción de recibir un impacto
+     * @return true si la pulga debe ser eliminada, false en caso contrario
+     */
+    public abstract boolean recibirImpacto();
+    
+    /**
+     * Hace que la pulga salte a una posición aleatoria
+     */
+    public void saltar(int maxX, int maxY) {
+        this.x = (int) (Math.random() * (maxX - ancho));
+        this.y = (int) (Math.random() * (maxY - alto));
+    }
+    
+    /**
+     * Obtiene el rectángulo de colisión de la pulga
+     */
+    public Rectangle obtenerRectangulo() {
+        return new Rectangle(x, y, ancho, alto);
+    }
+    
+    /**
+     * Verifica si esta pulga colisiona con otra
+     */
+    public boolean colisiona(Pulga otra) {
+        return obtenerRectangulo().intersects(otra.obtenerRectangulo());
+    }
+    
+    /**
+     * Obtiene la posición X
+     */
+    public int getX() {
+        return x;
+    }
+    
+    /**
+     * Obtiene la posición Y
+     */
+    public int getY() {
+        return y;
+    }
 }
