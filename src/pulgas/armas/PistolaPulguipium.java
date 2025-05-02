9510b8683a1ad049c37954d86b0d99f6968779b2
Juan José Trujillo <juanj.trujillo@autonoma.edu.co>
2025-05-02 15:11:35 -0500

    Se documento la clase PistolaPulguipium

--- src/pulgas/armas/PistolaPulguipium.java
+++ src/pulgas/armas/PistolaPulguipium.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package pulgas.armas;
+
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.util.List;
@@ -12,15 +13,22 @@ import pulgas.models.PulgaMutante;
 import pulgas.models.PulgaNormal;
 
 /**
- *
- * @author juans
+ * Implementación del arma "Pistola Pulguipium", que dispara en una pequeña
+ * área y afecta a una sola pulga que esté en el punto de impacto.
  */
 public class PistolaPulguipium implements Arma {
+
+    /**
+     * Ejecuta el disparo de la Pistola Pulguipium.
+     * El arma detecta si alguna pulga está en el área del disparo y actúa:
+     * si la pulga es eliminada, se suma al puntaje; si es mutante y sobrevive,
+     * se convierte en una pulga normal. Solo afecta a una pulga por disparo.
+     */
     @Override
     public int atacar(List<Pulga> pulgas, int x, int y, GestorPuntaje gestorPuntaje, Image imgPulgaNormal) {
-        Rectangle puntoDisparo = new Rectangle(x-5, y-5, 10, 10);
+        Rectangle puntoDisparo = new Rectangle(x - 5, y - 5, 10, 10);
         int pulgasEliminadas = 0;
-        
+
         for (int i = 0; i < pulgas.size(); i++) {
             Pulga pulga = pulgas.get(i);
             if (pulga.obtenerRectangulo().intersects(puntoDisparo)) {
@@ -36,7 +44,7 @@ public class PistolaPulguipium implements Arma {
                 break; // Solo afecta a una pulga
             }
         }
-        
+
         return pulgasEliminadas;
     }
 }
