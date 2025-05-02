9c1f708081de2a134e895a1dc9f5b25675c5b5ee
Juan José Trujillo <juanj.trujillo@autonoma.edu.co>
2025-05-02 15:16:53 -0500

    Se documento la clase GeneradorPulgas

--- src/pulgas/control/GeneradorPulgas.java
+++ src/pulgas/control/GeneradorPulgas.java
@@ -10,34 +10,44 @@ package pulgas.control;
  *
  */
 
+/**
+ * Hilo encargado de generar pulgas normales y mutantes periódicamente
+ * mientras el juego esté en ejecución.
+ */
 public class GeneradorPulgas extends Thread {
     private CampoBatalla campoBatalla;
     private boolean ejecutando;
-    
+
+    /**
+     * Constructor que recibe el campo de batalla donde se agregarán las pulgas.
+     */
     public GeneradorPulgas(CampoBatalla campoBatalla) {
         this.campoBatalla = campoBatalla;
         this.ejecutando = true;
     }
-    
+
+    /**
+     * Ejecuta el hilo que agrega pulgas normales cada 5 segundos y mutantes cada 10 segundos.
+     */
     @Override
     public void run() {
         try {
             int contadorNormal = 0;
             int contadorMutante = 0;
-            
+
             while (ejecutando) {
-                Thread.sleep(1000); 
-                
+                Thread.sleep(1000); // Espera 1 segundo
+
                 contadorNormal++;
                 contadorMutante++;
-                
-                if (contadorNormal >= 5) { 
-                    campoBatalla.agregarPulgaNormal();
+
+                if (contadorNormal >= 5) {
+                    campoBatalla.agregarPulgaNormal(); // Cada 5 segundos
                     contadorNormal = 0;
                 }
-                
-                if (contadorMutante >= 10) { 
-                    campoBatalla.agregarPulgaMutante();
+
+                if (contadorMutante >= 10) {
+                    campoBatalla.agregarPulgaMutante(); // Cada 10 segundos
                     contadorMutante = 0;
                 }
             }
@@ -45,9 +55,12 @@ public class GeneradorPulgas extends Thread {
             ejecutando = false;
         }
     }
-    
+
+    /**
+     * Detiene la ejecución del hilo de generación de pulgas.
+     */
     public void detener() {
         ejecutando = false;
-        interrupt();
+        interrupt(); // Interrumpe si está dormido
     }
 }
