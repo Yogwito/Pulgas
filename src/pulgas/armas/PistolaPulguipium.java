1652cbf6c5b06dc950a316d75c20db07cb0d5e3a
Sebastian Arias <juansearias21@gmail.com>
2025-05-07 15:10:00 -0500

    pistola pulguipum actualizada

--- src/pulgas/armas/PistolaPulguipium.java
+++ src/pulgas/armas/PistolaPulguipium.java
@@ -7,10 +7,7 @@ package pulgas.armas;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.util.List;
-import pulgas.models.GestorPuntaje;
-import pulgas.models.Pulga;
-import pulgas.models.PulgaMutante;
-import pulgas.models.PulgaNormal;
+import pulgas.models.*;
 /**
  *
  * @author juans & Trujirend & JJDaza
@@ -30,25 +27,24 @@ public class PistolaPulguipium implements Arma {
      */
     @Override
     public int atacar(List<Pulga> pulgas, int x, int y, GestorPuntaje gestorPuntaje, Image imgPulgaNormal) {
-        Rectangle puntoDisparo = new Rectangle(x - 5, y - 5, 10, 10);
-        int pulgasEliminadas = 0;
-
+        Rectangle puntoDisparo = new Rectangle(x - 10, y - 10, 20, 20);
         for (int i = 0; i < pulgas.size(); i++) {
             Pulga pulga = pulgas.get(i);
             if (pulga.obtenerRectangulo().intersects(puntoDisparo)) {
                 if (pulga.recibirImpacto()) {
                     pulgas.remove(i);
-                    pulgasEliminadas++;
                     gestorPuntaje.aumentarPuntaje(1);
                 } else if (pulga instanceof PulgaMutante) {
-                    // Convertir pulga mutante en normal
-                    PulgaNormal pulgaNormal = new PulgaNormal(pulga.getX(), pulga.getY(), imgPulgaNormal);
-                    pulgas.set(i, pulgaNormal);
+                    if (imgPulgaNormal != null) {
+                        PulgaNormal pulgaNormal = new PulgaNormal(
+                            pulga.getX(), pulga.getY(), imgPulgaNormal
+                        );
+                        pulgas.set(i, pulgaNormal);
+                    }
                 }
-                break; // Solo afecta a una pulga
+                break;
             }
         }
-
-        return pulgasEliminadas;
+        return 0;
     }
 }
